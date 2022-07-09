2aa560a58f29b654aa3a584dcc692302f5e8d491
mukesh-cel <52950664+mukesh-cel@users.noreply.github.com>
2022-07-09 22:42:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,10 @@ server.listen(port, (error) => {
     if (error) throw error;
     console.log(`Your app is running on port ${port}`);
 });
+
+
+POD_NAME=$(kubectl get pods | grep kubernetes-101-* |awk '{ print $1}')
+
+4.2	In order to port forward pod’s port to localhost run the following command:
+
+kubectl port-forward $POD_NAME 3001:3000
